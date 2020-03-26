83a4efce6bc096e915ef1c0f9590537e72d153b9
patr0nus <dk4rest@gmail.com>
2020-03-26 21:49:21 +0800

    Proof-of-concept implementation of event loop intergration for macOS

--- src/node_api_embedding.h
+++ src/node_api_embedding.h
@@ -5,17 +5,19 @@
 
 EXTERN_C_START
 
+typedef void* tick_data_t;
+
 typedef struct {
   const char* script;
   napi_addon_register_func reg_func;
   int instance_argc;
   const char** instance_argv;
   void(*loop_func)();
-  void(*on_tick_func);
+  void(*on_tick_func)(tick_data_t tick_data);
 } node_init_info;
 
 int node_main(const node_init_info* init_info);
-int node_tick();
+void node_tick(tick_data_t tick_data);
 
 
 EXTERN_C_END
